8c5b07bdbb7008f001cca9fdf0762ec030d52e1d
assassincreedv <assassincreedv@hotmail.com>
2019-07-11 17:34:16 -0400

    ProductRepository

---
+++ eCommerceMVC/Repository/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eCommerceMVC.Models;
+using eCommerceMVC.Repository;
+
+namespace eCommerceMVC.Repository
+{
+    public class ProductRepository:GenericRepository<Product>, IProductRepository
+    {
+
+        public ProductRepository() {
+        }
+    }
+}
